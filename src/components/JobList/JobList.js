92c94b08bf8064a0dd9e97b08bf16a6c7f54021c
David Watson <davidlwatsonjr@users.noreply.github.com>
2024-03-11 16:39:31 -0400

    Add loading indicator

--- src/components/JobList/JobList.js
+++ src/components/JobList/JobList.js
@@ -1,20 +1,31 @@
 import { useEffect, useState } from "react";
 import "./JobList.css";
+import { LinearProgress } from "@mui/material";
 
 function JobList() {
-  const [jobList, setJobList] = useState(localStorage.getItem("jobList") ? JSON.parse(localStorage.getItem("jobList")) : []);
+  const [isLoadingJobs, setIsLoadingJobs] = useState(true);
+  const [jobList, setJobList] = useState(
+    localStorage.getItem("jobList")
+      ? JSON.parse(localStorage.getItem("jobList"))
+      : [],
+  );
 
   useEffect(() => {
     (async () => {
+      setIsLoadingJobs(true);
       const response = await fetch("https://jobs.davidlwatsonjr.com/jobs");
       const { jobs } = await response.json();
       localStorage.setItem("jobList", JSON.stringify(jobs));
       setJobList(jobs);
+      setIsLoadingJobs(false);
     })();
   }, []);
 
   return (
     <div data-testid="job-list">
+      <div className="JobsLoadingBar">
+        {isLoadingJobs && <LinearProgress />}
+      </div>
       <h1>Job List</h1>
       <ul data-testid="job-list-ul" className="JobList">
         {jobList?.map?.((job) => (
